70baf18d4249120844d1b04406793809216c210b
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 23:39:46 +0200

    feat(all): initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import {WebpackConfig, get} from '@easy-webpack/core'
+import * as webpack from 'webpack'
+
+export function regenerator() {
+  return function regenerator(this: WebpackConfig): WebpackConfig {
+    return {
+      plugins: [
+        new webpack.ProvidePlugin({
+          'regeneratorRuntime': 'regenerator-runtime', // required for await-async
+        })
+      ].concat(get(this, 'plugins', []))
+    }
+  }
+}
